-Fixed loss coefficients -Start for exponential moving average as the initial rewrad average -Changed ent coeff

rllib/agents/dreamer/dreamer_model.py
@@ -25,14 +25,14 @@ class Reshape(nn.Module):
         return x.view(*self.shape)
 
 
-class EMABatchTime:
+class EMA: #BatchTime:
     '''
     This class is mainly targeting EMA of losses in batched temporal data
     '''
     def __init__(self, decay: float = 0.9):
 
         self.decay = decay
-        self.b_k = torch.Tensor([0.])
+        self.b_k = torch.Tensor([float('nan')])
 
     def update(self, new_value):
         """
@@ -42,9 +42,13 @@ class EMABatchTime:
         new_value_c = new_value.detach().clone()
 
         with torch.no_grad():
-            if len(new_value.size()) > 1:
-                new_value_c=new_value_c.mean(tuple(range(2, len(new_value.size()))))
+            #if len(new_value.size()) > 1:
+            #    new_value_c=new_value_c.mean(tuple(range(2, len(new_value.size()))))
                 #print(new_value_c)
+            new_value_c=new_value_c.mean()
+            if torch.isnan(self.b_k):
+              self.b_k = new_value_c
+              return self.b_k
             self.b_k = self.decay*self.b_k + (1-self.decay)*new_value_c
 
         return self.b_k
@@ -700,7 +704,7 @@ class DreamerModel(TorchModelV2, nn.Module):
                                   self.hidden_size)
         self.state = None
 
-        self.ema = EMABatchTime(self.decay)
+        self.ema = EMA(self.decay)#EMABatchTime(self.decay)
         self.device = (torch.device("cuda")
                        if torch.cuda.is_available() else torch.device("cpu"))
 

rllib/agents/dreamer/dreamer_torch_policy.py
@@ -23,7 +23,7 @@ def compute_dreamer_loss(obs,
                          discount=0.99,
                          lambda_=0.95,
                          kl_coeff=1.0,
-                         ent_coeff=1.0,
+                         ent_coeff=0.1,
                          rei_coeff=1.0,
                          free_nats=3.0,
                          log=False):
@@ -37,6 +37,8 @@ def compute_dreamer_loss(obs,
             discount (float): Discount
             lambda_ (float): Lambda, like in GAE
             kl_coeff (float): KL Coefficient for Divergence loss in model loss
+            ent_coeff(float): Mask Entropy Coefficient for Mask Entropy loss in model loss
+            rei_coeff(float): Reinforce Coefficient for Reinforce loss in model loss
             free_nats (float): Threshold for minimum divergence in model loss
             log (bool): If log, generate gifs
         """
@@ -74,10 +76,11 @@ def compute_dreamer_loss(obs,
     reinforce_reward_ema = model.ema.update(reinforce_reward)
     mask_logp = mask_logp_x + mask_logp_h
     mask_reinforce_loss = -torch.mean((reinforce_reward - reinforce_reward_ema) * mask_logp)
-    mask_entropy_loss = -(torch.mean(mask_entropy_x) + torch.mean(mask_entropy_x))
+    mask_entropy_loss = -(torch.mean(mask_entropy_x) + torch.mean(mask_entropy_h))
     image_loss = torch.mean(image_loss)
     reward_loss = torch.mean(reward_loss)
-    model_loss = kl_coeff * div + reward_loss + image_loss + rei_coeff*mask_entropy_loss + ent_coeff * mask_reinforce_loss# Actor Loss
+    model_loss = kl_coeff * div + reward_loss + image_loss +\
+                 ent_coeff*mask_entropy_loss + rei_coeff*mask_reinforce_loss
     # [imagine_horizon, batch_length*batch_size, feature_size]
     with torch.no_grad():
         actor_states = [v.detach() for v in post]
@@ -121,6 +124,8 @@ def compute_dreamer_loss(obs,
         "critic_loss": critic_loss,
         "prior_ent": prior_ent,
         "post_ent": post_ent,
+        "mask_ent_loss": mask_entropy_loss,
+        "mask_reinforce_loss": mask_reinforce_loss
     }
 
     if log_gif is not None: