36e51b8b64bac2544475890035e4129648debc7f
Karim174 <karimyehiamct@hotmail.com>
2021-08-04 18:24:09 +0200

    adjsuted size updates and removed the decision of whether to randomize or not

--- rllib/env/randomized_dm_control.py
+++ rllib/env/randomized_dm_control.py
@@ -61,32 +61,29 @@ class RDMCEnv(DMCEnv):
             self._env.physics.named.model.geom_size[self.bodies] = self.orig_geom_size
 
         time_step = self._env.reset()
-
+        density = 1
+        # sample density
         if self.rng.random() < 0.5:
+            density = self.rng.uniform(0.5, 1.5)
+        sym_bodies = set(
+            body.split('_', 1)[1] if body.find("_") != -1 else body for body in self.bodies)  # or seen_add(body))
+        change_mask = self.rng.binomial(1, p=0.5, size=len(sym_bodies))
+        steps = np.arange(-0.5, 0.75, 0.25)
+        steps = np.concatenate([steps[:steps.shape[0] // 2], steps[steps.shape[0] // 2 + 1:]])
+        change_values = self.rng.choice(steps, len(sym_bodies))
+        change_factor = 1 + change_values * change_mask
+        sym_bdy_cng_dict = dict(zip(sym_bodies, change_factor))
+        bm_bdy_cng = np.array(
+            [sym_bdy_cng_dict[body.split('_', 1)[1]] if body.find("_") != -1 else sym_bdy_cng_dict[body] for body in
+             self.bodies])
 
-            density = 1
-            # sample density
-            if self.rng.random() < 0.5:
-                density = self.rng.uniform(0.5, 1.5)
-            sym_bodies = set(
-                body.split('_', 1)[1] if body.find("_") != -1 else body for body in self.bodies)  # or seen_add(body))
-            change_mask = self.rng.binomial(1, p=0.5, size=len(sym_bodies))
-            steps = np.arange(-0.5, 0.75, 0.25)
-            steps = np.concatenate([steps[:steps.shape[0] // 2], steps[steps.shape[0] // 2 + 1:]])
-            change_values = self.rng.choice(steps, len(sym_bodies))
-            change_factor = 1 + change_values * change_mask
-            sym_bdy_cng_dict = dict(zip(sym_bodies, change_factor))
-            bm_bdy_cng = np.array(
-                [sym_bdy_cng_dict[body.split('_', 1)[1]] if body.find("_") != -1 else sym_bdy_cng_dict[body] for body in
-                 self.bodies])
+        bodies_mass = self.orig_body_mass * bm_bdy_cng
+        density = np.array([1 if i == 1 else density for i in bm_bdy_cng])
+        bodies_sizes = self.orig_geom_size * (1 + density * (bm_bdy_cng-1))[:, None]
 
-            bodies_mass = self.orig_body_mass * bm_bdy_cng
-            density = np.array([1 if i == 1 else density for i in bm_bdy_cng])
-            bodies_sizes = self.orig_geom_size * (density * bm_bdy_cng)[:, None]
-
-            with self._env.physics.reset_context():
-                self._env.physics.named.model.body_mass[self.bodies] = bodies_mass
-                self._env.physics.named.model.geom_size[self.bodies] = bodies_sizes
+        with self._env.physics.reset_context():
+            self._env.physics.named.model.body_mass[self.bodies] = bodies_mass
+            self._env.physics.named.model.geom_size[self.bodies] = bodies_sizes
 
         self.current_state = _flatten_obs(time_step.observation)
         obs = self._get_obs(time_step)
